ffe76b6a92a667b391ab02e81b4515d80f6997dc
samso <samso@example.com>
2026-09-04 10:54:38 +0800

    更新AL2

--- src/App.jsx
+++ src/App.jsx
@@ -44,20 +44,20 @@ const appId = typeof __app_id !== 'undefined' ? __app_id : 'default-shift-app';
 // ==========================================
 // 🔧 系統核心設定 (每次開新更表只需修改這裡！)
 // ==========================================
-const SCHEDULE_START_DATE = '2026-09-14'; // 👈👈👈 修改日期就在這裡！！
+const SCHEDULE_START_DATE = '2026-07-20'; // 👈👈👈 修改日期就在這裡！！
 const PERIOD_ID = `period_${SCHEDULE_START_DATE}`; 
 
 const EMPLOYEES = [
   { id: 'emp_1', name: 'KCKB' },
-  { id: 'emp_2', name: 'KFW' },
-  { id: 'emp_3', name: 'CCKR' },
-  { id: 'emp_4', name: 'HMC' },
-  { id: 'emp_5', name: 'PL' },
-  { id: 'emp_6', name: 'CPY' },
-  { id: 'emp_7', name: 'RH' },
-  { id: 'emp_8', name: 'FHW' },
-  { id: 'emp_9', name: 'CTW' },
-  { id: 'emp_10', name: 'LHF' }
+  { id: 'emp_2', name: 'Leo' },
+  { id: 'emp_3', name: 'Ricky' },
+  { id: 'emp_4', name: 'Heyman' },
+  { id: 'emp_5', name: 'Perlin' },
+  { id: 'emp_6', name: 'Paco' },
+  { id: 'emp_7', name: 'Ryan' },
+  { id: 'emp_8', name: 'Vincent' },
+  { id: 'emp_9', name: 'Wai' },
+  { id: 'emp_10', name: 'Hugo' }
 ];
 
 // 生成 14 天空白更表結構 (加入 AL)
